0efed6ce4763e79ce8fa02b289d0d6715ade193a
bradenmweight <braden.m.weight@gmail.com>
2024-01-10 13:04:52 -0500

    Fixed final walker bug

--- DQMC/src/QMC.py
+++ DQMC/src/QMC.py
@@ -16,13 +16,16 @@ def DMC( PARAM, positions=None ):
         alpha     = PARAM["alpha_equilibration"]
 
         positions = np.zeros( (particles,num_walkers,dimension) )
-        for Ri,R in enumerate( R_NUC ):
-            for dim in range(dimension):
-                NSTART = Ri*(num_walkers//len(R_NUC))
-                NEND   = (Ri+1)*(num_walkers//len(R_NUC))
-                #positions[:,NSTART:NEND,dim]  = np.random.normal(R[dim], 2, size=(particles,NEND-NSTART))
-                positions[:,NSTART:NEND,dim]  = (np.random.uniform(size=(particles,NEND-NSTART))*2-1)*20
-
+        # for Ri,R in enumerate( R_NUC ):
+        #     for dim in range(dimension):
+        #         NSTART = Ri*(num_walkers//len(R_NUC))
+        #         NEND   = (Ri+1)*(num_walkers//len(R_NUC))
+        #         print(NSTART, NEND, num_walkers)
+        #         positions[:,NSTART:NEND,dim]  = (np.random.uniform(size=(particles,NEND-NSTART))*2-1)*10
+        
+        for dim in range(dimension):
+            positions[:,:,dim]  = (np.random.uniform(size=(particles,num_walkers))*2-1)*10
+        
         # Initialize Cavity Photon
         if ( PARAM["DO_POLARITON"] == True ):
             PARAM["QC"] = np.random.uniform(size=(num_walkers))*2-1 # Assuming only a single dimension/mode # TODO
@@ -67,16 +70,19 @@ def DMC( PARAM, positions=None ):
         positions_old = positions
         potential_energies_old = get_potential(positions_old,PARAM)
 
-        energy_traj[step,0] = np.average( get_potential(positions,PARAM) )
-        energy_traj[step,1] = np.var( get_potential(positions,PARAM) )
-        energy_traj[step,2] = np.std( get_potential(positions,PARAM) )
+        energy_traj[step,0] = np.average( potential_energies_old )
+        print( step, potential_energies_old[-1], energy_traj[step,0] )
+        exit()
+        energy_traj[step,1] = np.var( potential_energies_old )
+        energy_traj[step,2] = np.std( potential_energies_old )
         energy_traj[step,3] = PARAM["E_TRIAL_GUESS"]
         energy_traj[step,4] = len(positions[0,:]) # Current number of walkers
 
         if ( PARAM["DO_POLARITON"] == True ):
             PARAM["QC_TRAJ"][:,step] = PARAM["QC"][:100]
 
-        if ( step % (num_steps//20) == 0 ):
+        #if ( step % (num_steps//20) == 0 ):
+        if ( step % 500 == 0 ):
             print ( f"Step = {step}, N = {len(positions[0,:,0])}, E_T = {round(energy_traj[step,3],3)}, <E> = {round(energy_traj[step,0],3)}" )
 
         # Propagate the walkers
@@ -86,10 +92,13 @@ def DMC( PARAM, positions=None ):
 
         # Compute the potential energy for each walker
         potential_energies     = get_potential(positions,PARAM)
+        print("TEST:", potential_energies)
         AVE_Pot                = (potential_energies+potential_energies_old)/2
 
         # Compute the weights of the walkers
         PARAM["E_TRIAL_GUESS"] = np.average( potential_energies_old ) + alpha * np.log( num_walkers / len(positions[0,:]) )
+
+        print ( AVE_Pot )
 
         #dE = potential_energies - PARAM["E_TRIAL_GUESS"]
         dE = AVE_Pot - PARAM["E_TRIAL_GUESS"]
